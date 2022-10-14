7ce0056365f28e779c2621a4d267501b2c584cd0
Karoline Carvalho <karoline.carvalho@valtech.com>
2022-10-14 15:37:57 -0300

    refactor: clean unused code

--- components/atoms/Heading/Heading.js
+++ components/atoms/Heading/Heading.js
@@ -6,10 +6,7 @@ export default function Heading({ size, color, weight, level, children }) {
   classList.push(styles[`heading--${size}`]);
   classList.push(styles[`heading--${color}`]);
   classList.push(styles[`heading--${weight}`]);
-  console.log(classList.join(" "));
-  {
-    /* <h1 className={classList.join(" ")}>hello</h1> */
-  }
-  let props = { className: classList.join(" ") };
+
+  const props = { className: classList.join(" ") };
   return React.createElement("h" + level, props, children);
 }
