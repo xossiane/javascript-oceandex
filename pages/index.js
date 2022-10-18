ab915ea74937999e5a4515e1c8ae66c025cc3132
xossiane <anejosi.lima@gmail.com>
2022-10-18 12:11:48 -0300

    Add: insert AnimalTag on main section

--- pages/index.js
+++ pages/index.js
@@ -2,17 +2,19 @@ import Arrow from "@atoms/Arrow/Arrow";
 import Heading from "@atoms/Heading/Heading";
 import Like from "@atoms/Like/Like";
 import Text from "@atoms/Text/Text";
+import AnimalsTag from "@molecules/AnimalsTag";
 
 export default function mainpage() {
   return (
     <>
       <Arrow white={false} />
       <div style={{ width: "100px" }}>
-        <Like liked={false} size={"1"} />
+        <Like liked={true} size={"1"} />
         <Text color="black" size="small" />
         <Heading level="3" size="large" color="white" weight="bold">
           teste
         </Heading>
+        <AnimalsTag></AnimalsTag>
       </div>
     </>
   );
