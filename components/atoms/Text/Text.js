9628e190a6d58d8d03bf4f4ef4c46be44d2510aa
xossiane <anejosi.lima@gmail.com>
2022-10-18 12:10:04 -0300

    Add: new props on Text

--- components/atoms/Text/Text.js
+++ components/atoms/Text/Text.js
@@ -1,10 +1,12 @@
 import styles from "./Text.module.scss";
 
-export default function Text({ size, color, weight }) {
+
+
+export default function Text({ size, color, weight, children, className}) {
   const classList = [];
   classList.push(styles[`text--${size}`]);
   classList.push(styles[`text--${color}`]);
   classList.push(styles[`text--${weight}`]);
 
-  return <p className={classList.join(" ")}>hello</p>;
+  return <p className={`${classList.join(" ")} ${className}`}>{children}</p>;
 }
