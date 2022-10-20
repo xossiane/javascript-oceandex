cc6c0cbb2e4140bff26afe7b12e01253eb039701
Karoline Carvalho <karoline.carvalho@valtech.com>
2022-10-20 10:21:04 -0300

    add: temporary imports and components

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,14 @@ import Arrow from "@atoms/Arrow/Arrow";
 import Heading from "@atoms/Heading/Heading";
 import Like from "@atoms/Like/Like";
 import Text from "@atoms/Text/Text";
+import AnimalsCard from "@molecules/AnimalsCard";
+
 import AnimalsTag from "@molecules/AnimalsTag";
 
+import animals from "oceananimals.json";
 export default function mainpage() {
+  const firstAnimal = animals[20];
+
   return (
     <>
       <Arrow white={false} />
@@ -14,7 +19,20 @@ export default function mainpage() {
         <Heading level="3" size="large" color="white" weight="bold">
           teste
         </Heading>
-        <AnimalsTag></AnimalsTag>
+
+        <div
+          style={{
+            width: "150px",
+            height: "110px",
+          }}
+        >
+          <AnimalsCard
+            name={firstAnimal.name}
+            order={firstAnimal.order}
+            phylum={firstAnimal.phylum}
+            src={firstAnimal.icon}
+          />
+        </div>
       </div>
     </>
   );
