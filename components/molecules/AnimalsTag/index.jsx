d4e99a2764c469bfb2ac7e0218b6c6d1d23ccb7a
Karoline Carvalho <karoline.carvalho@valtech.com>
2022-10-20 10:18:05 -0300

    refactor: add children prop in animalsTag

--- components/molecules/AnimalsTag/index.jsx
+++ components/molecules/AnimalsTag/index.jsx
@@ -1,37 +1,27 @@
-import React from 'react'
+import React from "react";
 import Text from "@atoms/Text/Text";
-import oceananimals from "../../../../javascript-oceandex/oceananimals.json"
+import oceananimals from "oceananimals.json";
 import styles from "./AnimalsTag.module.scss";
 
-
-function AnimalsTag(props) {
-    console.log(oceananimals)
-    const animals = oceananimals
-    console.log(animals.map((animal => animal.id)))
-    console.log(animals)
+function AnimalsTag({ children }) {
+  //console.log(oceananimals);
+  const animals = oceananimals;
+  //console.log(animals.map((animal) => animal.id));
+  //console.log(animals);
   return (
-  <>
-  
-    
-    
-    	<div className={styles[`AnimalsTag--bg`]}>
-      <Text className={styles[`AnimalsTag--text`]} color="black" size="1.2rem" key={animals.id}>this is us</Text>
-      
+    <>
+      <div className={styles[`AnimalsTag--bg`]}>
+        <Text
+          className={styles[`AnimalsTag--text`]}
+          color="black"
+          size="1.2rem"
+          key={animals.id}
+        >
+          {children}
+        </Text>
       </div>
-
-    
-    
-    
-   
-  
-
- 
-  
-    
     </>
-  )
- 
-  
+  );
 }
 
-export default AnimalsTag
+export default AnimalsTag;
