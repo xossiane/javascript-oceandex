8f900bfbbb2fe28879037134e9f615bcb36d4b7a
xossiane <anejosi.lima@gmail.com>
2022-10-27 17:31:43 -0300

    Add: style italic in Text component

--- components/atoms/Text/Text.js
+++ components/atoms/Text/Text.js
@@ -2,11 +2,12 @@ import styles from "./Text.module.scss";
 
 
 
-export default function Text({ size, color, weight, children, className}) {
+export default function Text({ size, color, weight, style, children, className}) {
   const classList = [];
   classList.push(styles[`text--${size}`]);
   classList.push(styles[`text--${color}`]);
   classList.push(styles[`text--${weight}`]);
+  classList.push(styles[`text--${style}`]);
 
   return <p className={`${classList.join(" ")} ${className}`}>{children}</p>;
 }
