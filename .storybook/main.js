2cfa346071770406d7019f6a6ea007234e3e1645
Kelly Cesário de Oliveira <kellycesario@gmail.com>
2022-10-31 16:41:18 -0300

    Add: storybook and a11y and html addons

---
+++ .storybook/main.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+  "stories": [
+    "../components/**/*.stories.mdx",
+    "../components/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/addon-a11y",
+    "@storybook-addon-html"
+  ],
+  "features": {
+    storyStoreV7: true,
+  },
+  "framework": "@storybook/react",
+  "core": {
+    "builder": "webpack5"
+  },
+
+  webpackFinal: async (config, { configType }) => {
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: ['style-loader', 'css-loader', 'sass-loader'],
+      include: path.resolve(__dirname, '../'),
+    });
+
+    return config;
+  },
+};
---
+++ components/atoms/Search/Search.stories.jsx
@@ -0,0 +1,10 @@
+import SearchIcon from './Search'
+
+export default {
+    title: "Atom/Search",
+    component: SearchIcon,
+    args: {}
+}
+
+export const Primary = () => <SearchIcon primary />;
+
