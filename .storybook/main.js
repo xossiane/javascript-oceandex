6d3181d6592c999cf3f6fccd6d807c9ce558cb98
Kelly Cesário de Oliveira <kellycesario@gmail.com>
2022-11-01 14:01:39 -0300

    Add: storybook-addon-html

--- .storybook/main.js
+++ .storybook/main.js
@@ -10,7 +10,7 @@ module.exports = {
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "@storybook/addon-a11y",
-    "@storybook-addon-html"
+    "@whitespace/storybook-addon-html"
   ],
   "features": {
     storyStoreV7: true,
