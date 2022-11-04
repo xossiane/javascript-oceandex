7b5a2de65d8a10f1716c8dd2abefb31b9b63c29d
Karoline Carvalho <karoline.carvalho@valtech.com>
2022-11-04 11:07:04 -0300

    add: useContentful hook

--- pages/index.jsx
+++ pages/index.jsx
@@ -8,9 +8,19 @@ import AnimalsCard from "@molecules/AnimalsCard";
 import AnimalsTag from "@molecules/AnimalsTag";
 
 import animals from "oceananimals.json";
-export default function mainpage() {
-  const firstAnimal = animals[20];
 
+import useContentful from "hooks/useContentful";
+import { useEffect, useState } from "react";
+
+export default function mainpage() {
+  //const firstAnimal = animals[20];
+  const { getAnimals } = useContentful();
+  const [oceanAnimals, setOceanAnimals] = useState([]);
+
+  useEffect(() => {
+    getAnimals().then((response) => setOceanAnimals(response));
+  }, []);
+  console.log(oceanAnimals);
   return (
     <>
       <Arrow white={false} />
@@ -21,6 +31,24 @@ export default function mainpage() {
           teste
         </Heading>
         <AnimalsTag></AnimalsTag>
+        <Background></Background>
+        <AnimalsTag>teste</AnimalsTag>
+        <div
+          style={{
+            width: "150px",
+            height: "110px",
+          }}
+        >
+          {oceanAnimals.map((animal, index) => (
+            <AnimalsCard
+              key={index}
+              name={animal.name}
+              order={animal.classification.order}
+              phylum={animal.classification.phylum}
+              src={animal.image.file.url}
+            />
+          ))}
+        </div>
       </div>
     </>
   );
