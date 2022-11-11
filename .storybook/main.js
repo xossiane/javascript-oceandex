3ff9ee963a91dc3b81755cf2cd8da6d457900515
kellycesario <kellycesario@gmail.com>
2022-11-11 08:17:14 -0300

    Fix: storybook alias issue

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,32 +1,44 @@
-const path = require('path');
+const path = require("path");
+// const JsConfigPathsPlugin = require("jsconfig-paths-webpack-plugin");
 
 module.exports = {
-  "stories": [
+  stories: [
     "../components/**/*.stories.mdx",
-    "../components/**/*.stories.@(js|jsx|ts|tsx)"
+    "../components/**/*.stories.@(js|jsx|ts|tsx)",
   ],
-  "addons": [
-    "@storybook/addon-links",
+  addons: [
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "@storybook/addon-a11y",
-    "@whitespace/storybook-addon-html"
   ],
-  "features": {
+  features: {
     storyStoreV7: true,
   },
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "webpack5"
+  framework: "@storybook/react",
+  core: {
+    builder: "webpack5",
   },
 
   webpackFinal: async (config, { configType }) => {
     config.module.rules.push({
       test: /\.scss$/,
-      use: ['style-loader', 'css-loader', 'sass-loader'],
-      include: path.resolve(__dirname, '../'),
+      use: ["style-loader", "css-loader", "sass-loader"],
+      include: path.resolve(__dirname, "../"),
     });
-
+    // config.resolve.plugins = config.resolve.plugins || [];
+    // config.resolve.plugins.push(
+    //   new JsConfigPathsPlugin({
+    //     configFile: path.resolve(__dirname, "../jsconfig.json"),
+    //   })
+    // );
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(__dirname, "../src"),
+      "@styles": path.resolve(__dirname, "../styles/"),
+      "@atoms": path.resolve(__dirname, "../components/atoms/"),
+      "@molecules": path.resolve(__dirname, "../components/molecules/"),
+      "@organisms": path.resolve(__dirname, "../components/organisms/"),
+    };
     return config;
   },
 };
