8af51dd296d2a6750cf1487b6985c6984db9abd3
elainemattje <55418000+elainemattje@users.noreply.github.com>
2022-11-11 12:16:26 -0300

    buttons added on home page

--- pages/home.jsx
+++ pages/home.jsx
@@ -1,24 +1,84 @@
 import styles from "../styles/home.module.scss";
 import Background from "@atoms/Background";
 import Heading from "@atoms/Heading/Heading";
+import Button from "@atoms/Button/Button.jsx";
+import Text from "@atoms/Text/Text";
 
 function Home() {
     return (
-        <>
-            <main className={styles.container}>
-                <div className={styles.container}>
-                    <Heading level="1" size="large" color="black" weight="bold">
-                        What marine creature are you looking for?
-                    </Heading>
-                    <div>
-                    </div>
-
-                        
-                </div>
+        <div className={styles[`Home`]}>
+            <main className={styles[`Home__container`]}>
+                <Heading level="1" size="Xlarge" color="black" weight="bold">
+                    What marine creature are you looking for?
+                </Heading>
+                <section className={styles[`Home__content`]}>
+                    <Button
+                        size="medium"
+                        color="color-400"
+                        fontsize="fontsmall"
+                        className={styles[`Home__buttons--unit`]}
+                        >
+                        <Text color="white" size="small">
+                            OceanDex
+                        </Text>{" "}
+                    </Button>
+                    <Button
+                        size="medium"
+                        color="color-800"
+                        fontsize="fontsmall"
+                        className={styles[`Home__buttons--unit`]}
+                        >
+                        <Text color="white" size="small">
+                            General Curiosities
+                        </Text>{" "}
+                    </Button>
+                    <Button
+                        size="medium"
+                        color="color-500"
+                        fontsize="fontsmall"
+                        className={styles[`Home__buttons--unit`]}
+                        >
+                        <Text color="white" size="small">
+                            Ocean Worldwide
+                        </Text>{" "}
+                    </Button>
+                    <Button
+                        size="medium"
+                        color="color-700"
+                        fontsize="fontsmall"
+                        className={styles[`Home__buttons--unit`]}
+                        >
+                        <Text color="white" size="small">
+                            Trophic Level
+                        </Text>{" "}
+                    </Button>
+                    <Button
+                        size="medium"
+                        color="color-200"
+                        fontsize="fontsmall"
+                        className={styles[`Home__buttons--unit`]}
+                        >
+                        <Text color="white" size="small">
+                            Favorite Animals
+                        </Text>{" "}
+                    </Button>
+                    <Button
+                        size="medium"
+                        color="color-300"
+                        fontsize="fontsmall"
+                        className={styles[`Home__buttons--unit`]}
+                        >
+                        <Text color="white" size="small">
+                            Who are you in the oceanDex?
+                        </Text>{" "}
+                    </Button>
+                </section>                        
             </main>
-            <Background/>
-        </>
-    );
+            <section className={styles[`Home__background`]}>
+                <Background />
+            </section>
+        </div>
+    )
 }
 
 export default Home;
