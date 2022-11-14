cdab2580a3e6e8696ec54d20fed7842409d8edaa
xossiane <anejosi.lima@gmail.com>
2022-11-14 16:12:17 -0300

    Del: OceanButtons page

--- components/organisms/OceanWorldwide/OceanWorldwide.stories.jsx
+++
@@ -1,9 +0,0 @@
-import OceanWorldwide from './OceanWorldwide'
-
-export default {
-    title: "Organisms/OceanWorldwide",
-    component: OceanWorldwide,
-    args: {}
-}
-
-export const Primary = () => <OceanWorldwide primary />;
