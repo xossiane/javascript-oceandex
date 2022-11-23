c4be65e5a6eafdcbfe0d802e07b52fc17f9f8d48
xossiane <anejosi.lima@gmail.com>
2022-11-23 18:33:06 -0300

    Add: Page about

---
+++ pages/About/About.stories.jsx
@@ -0,0 +1,9 @@
+import About from './About'
+
+export default {
+    title: "Pages/About",
+    component: About,
+    args: {}
+}
+
+export const Primary = () => <About primary />;
---
+++ pages/About/index.jsx
@@ -0,0 +1,56 @@
+import Arrow from '@atoms/Arrow/Arrow';
+import Heading from '@atoms/Heading/Heading';
+import Like from '@atoms/Like/Like';
+import AnimalsTag from '@molecules/AnimalsTag/AnimalsTag';
+import TabsDemo from '@organisms/Tabs/Tabs';
+
+import styles from './About.module.scss'
+
+export default function AboutPage() {
+  return (
+    <>
+      <div className={styles[`AboutPage__Container`]}>
+      <img
+        className={styles[`AboutPage__Header--rectangle`]}
+        src="assets/images/extras/Rectangle.png"
+        alt=""
+      />
+      <img
+        className={styles[`AboutPage__Header--dotted`]}
+        src="assets/images/extras/dotted.png"
+        alt=""
+      />
+
+      <header className={styles[`AboutPage__Header`]}>
+        <Arrow href="/Oceandex" white={true}></Arrow>
+        
+        
+        
+      </header>
+      <section className={styles[`AboutPage__Header--info`]}>
+        <Heading
+          level="1"
+          className={styles[`AboutPage__Header--text`]}
+          size="Xlarge"
+          color="white"
+        >
+          {" "}
+          Octopus{" "}
+        </Heading>
+        <section className={styles[`AboutPage__Header--tag`]}> 
+        <AnimalsTag label={"Octopoda"}></AnimalsTag> <AnimalsTag label={"Mollusca"}></AnimalsTag>
+        </section>
+        <img src="assets/images/icons/16-octopus.png" className={styles[`AboutPage__Header--icon`]} alt="animal icon"></img>
+        </section>
+
+        </div>
+        
+        
+      <div className={styles[`AboutPage__Container--bottom`]}>
+        <TabsDemo>
+          
+        </TabsDemo>
+        </div>
+    </>
+  );
+}
