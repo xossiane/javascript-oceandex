63f95221805c2f0de83df85040e92db640d5304e
xossiane <anejosi.lima@gmail.com>
2022-11-24 13:51:36 -0300

    Add: About Content organism

---
+++ components/organisms/AboutContent/AboutContent.stories.jsx
@@ -0,0 +1,9 @@
+import AboutContent from './AboutContent'
+
+export default {
+    title: "organisms/AboutContent",
+    component: AboutContent,
+    args: {}
+}
+
+export const Primary = () => <AboutContent primary />;
