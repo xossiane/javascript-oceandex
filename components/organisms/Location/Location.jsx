af58202e37a2b3623b54cfa4614512e3ef4bdee5
xossiane <anejosi.lima@gmail.com>
2022-11-24 15:49:44 -0300

    Add: Location tab

---
+++ components/organisms/Location/Location.jsx
@@ -0,0 +1,9 @@
+import styles from "./Location.module.scss"
+
+export default function Location() {
+  return (
+    <article>
+        <img src="assets/images/realAnimals/16-octopus.jpg"  className={styles[`Location__img`]}  alt="animal"></img>
+    </article>
+  )
+}
