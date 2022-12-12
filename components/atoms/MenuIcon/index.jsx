638fbe3f903802ddb3e28a828f99920bdc1db7fb
kelly-cesario_valtech <kellycesario@gmail.com>
2022-12-12 12:52:08 -0300

    Add: create the MenuIcon atom

---
+++ components/atoms/MenuIcon/index.jsx
@@ -0,0 +1,36 @@
+import Link from "next/link";
+import styles from "./styles.module.scss";
+
+export default function MenuIcon() {
+  return (
+    <svg
+      width="32"
+      height="32"
+      viewBox="0 0 32 32"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M5 16H27"
+        stroke="black"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M5 8H27"
+        stroke="black"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M5 24H27"
+        stroke="black"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+}
