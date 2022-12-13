d26dd049e1433c6fec228ae23242b48daf26aec8
Karoline Carvalho <karoline.carvalho@valtech.com>
2022-12-13 10:28:40 -0300

    refactor: heading and text sizes

--- components/atoms/Heading/index.jsx
+++ components/atoms/Heading/index.jsx
@@ -2,19 +2,19 @@ import styles from "./styles.module.scss";
 import React from "react";
 
 export default function Heading({
-  size,
+  // size,
   color,
   weight,
   level,
   style,
-  className,
   children,
 }) {
   const classList = [];
-  classList.push(styles[`heading--${size}`]);
+  //classList.push(styles[`heading--${size}`]);
   classList.push(styles[`heading--${color}`]);
   classList.push(styles[`heading--${weight}`]);
   classList.push(styles[`heading--${style}`]);
+  classList.push(styles[`heading--H${level}`]);
 
   const props = { className: classList.join(" ") };
   return React.createElement("h" + level, props, children);
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,12 @@
+import Text from "@atoms/Text";
 import GeneralCuriositiesPage from "./GeneralCuriosities";
 
 export default function MainPage() {
   return (
     <>
+      <Text size="small" weight="italic">
+        Teste Text
+      </Text>
       <GeneralCuriositiesPage></GeneralCuriositiesPage>
     </>
   );
