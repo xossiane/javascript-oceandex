2ddbc4c9831fa1b7fe775b07a1864efa22d68a96
Karoline Carvalho <karoline.carvalho@valtech.com>
2022-12-13 16:26:32 -0300

    refactor: text and heading sizes

--- components/atoms/Heading/index.jsx
+++ components/atoms/Heading/index.jsx
@@ -2,7 +2,7 @@ import styles from "./styles.module.scss";
 import React from "react";
 
 export default function Heading({
-  // size,
+  size = "",
   color,
   weight,
   level,
@@ -10,7 +10,7 @@ export default function Heading({
   children,
 }) {
   const classList = [];
-  //classList.push(styles[`heading--${size}`]);
+  classList.push(styles[`heading--${size}`]);
   classList.push(styles[`heading--${color}`]);
   classList.push(styles[`heading--${weight}`]);
   classList.push(styles[`heading--${style}`]);
