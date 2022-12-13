a450d027cad3c38a91cfccf8c836f9807995119c
Karoline Carvalho <karoline.carvalho@valtech.com>
2022-12-13 09:17:16 -0300

    refactor: contentful hook

--- hooks/useContentful.js
+++ hooks/useContentful.js
@@ -9,10 +9,35 @@ const useContentful = () => {
 
   const getAnimals = async () => {
     try {
+      const animalsList = client.getEntries({
+        content_type: "animal",
+        select: "fields",
+        "fields.id": "1",
+      });
+      const sanitizedList = (await animalsList).items.map((item) => {
+        const classification = item.fields.classification.fields;
+        const image = item.fields.image.fields.file.url;
+        //const curiosity = item.fields.curiosity.fields.description;
+        const location = {
+          oceanName: item.fields.location.content[0].content[0].value,
+          oceanFile:
+            item.fields.location.content[1].data.target.fields.file.url,
+        };
+
+        return { ...item.fields, image, location, classification };
+      });
+
+      return sanitizedList;
+    } catch (error) {
+      console.log(`Error fetching content: ${error}`);
+    }
+  };
+  /* const getAnimals = async () => {
+    try {
       const entries = await client.getEntries({
         content_type: "animal",
         select: "fields",
-        "fields.teste": "true",
+        //"fields.teste": "true",
       });
       const animals = [];
       const sanitizedAnimal = entries.items.map((item) => {
@@ -78,7 +103,8 @@ const useContentful = () => {
     }
   };
 
-  return { getAnimals, getAnimal };
+  return { getAnimals, getAnimal }; */
+  return { getAnimals };
 };
 
 export default useContentful;
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,21 @@
 import Head from "next/head";
 import GeneralCuriositiesPage from "./GeneralCuriosities";
 
+import { useEffect, useState } from "react";
+
+import useContentful from "../hooks/useContentful";
+
 export default function MainPage() {
+  const { getAnimals } = useContentful();
+  const [model, setModel] = useState();
+  const [loading, setLoading] = useState();
+
+  useEffect(() => {
+    getAnimals().then((response) => {
+      setModel(response);
+    });
+  }, []);
+  console.log(model);
   return (
     <div>
       <Head>
