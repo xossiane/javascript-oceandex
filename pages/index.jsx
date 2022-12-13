a9c102b1877d96c50cf44876ec17f0088b5fa81f
Karoline Carvalho <karoline.carvalho@valtech.com>
2022-12-13 10:29:54 -0300

    refactor: remove mock text

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,9 +4,6 @@ import GeneralCuriositiesPage from "./GeneralCuriosities";
 export default function MainPage() {
   return (
     <>
-      <Text size="small" weight="italic">
-        Teste Text
-      </Text>
       <GeneralCuriositiesPage></GeneralCuriositiesPage>
     </>
   );
