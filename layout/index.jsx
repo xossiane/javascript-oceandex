abb2d0e3dc124b1fd7eb1153ba18e3c0685fc2be
Matthew Nascimento <matthewromie@hotmail.com>
2022-12-23 11:50:52 -0300

    fix: background

--- layout/index.jsx
+++ layout/index.jsx
@@ -8,10 +8,12 @@ export default function Layout({ children }) {
     <>
       <Header />
       <div className={styles[`content`]}>{children}</div>
+      <>
         <div className={styles[`Background__ContainerBackgroundImage`]}>
           <img className={styles[`Background__ContainerBackgroundImage__BackgroundImage`]} src="/assets/images/extras/backgroundLarge2.png" alt="" />
           <img className={styles[`Background__ContainerBackgroundImage__BackgroundImageMobile`]} src="/assets/images/extras/background.png" alt="" />
         </div>
+        </>
       <Footer />
     </>
   );
