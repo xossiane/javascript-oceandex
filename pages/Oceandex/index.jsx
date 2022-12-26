79da2fb37a28c85819f20e9e4fe916bb0e7f6729
elainemattjevt <elaine.mattje@valtech.com>
2022-12-26 11:46:07 -0300

    About page and footer

--- pages/Oceandex/index.jsx
+++ pages/Oceandex/index.jsx
@@ -5,7 +5,6 @@ import { useEffect, useState, useCallback } from "react";
 import useContentful from "../../hooks/useContentful";
 
 import Heading from "@atoms/Heading";
-import Arrow from "@atoms/Arrow";
 import Input from "@molecules/Input";
 import Text from "@atoms/Text";
 import AnimalsCard from "@molecules/AnimalsCard";
@@ -86,9 +85,6 @@ const index = ({ data }) => {
   return (
     <>
       <div className={styles[`Oceandex__Container`]}>
-        <span className={styles[`Oceandex__Arrow`]}>
-          <Arrow direction="left" href="/" white={false} />
-        </span>
         <header className={styles[`Oceandex__Header`]}>
           <Heading
             level="1"
