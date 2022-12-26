cdb66a2a08609ea352bfe2154b1450dc756c9684
Karoline Carvalho <karoline.carvalho@valtech.com>
2022-12-26 11:58:15 -0300

    refactor: authors component

--- components/molecules/Authors/index.jsx
+++ components/molecules/Authors/index.jsx
@@ -9,22 +9,51 @@ export default function AuthorsMolecule({
   githubLink,
 }) {
   return (
-    <article className={styles.authors}>
-      <img src={picture} alt="" className={styles.authors__picture} />
-      <p className={styles.authors__name}> {name} </p>
-      <p className={styles.authors__job}>{job} </p>
-      <div className={styles.authors__socialMedia}>
-        <GlobalSvg
-          href={linkedinLink}
-          external
-          iconPath="/assets/images/svg/LinkedinLogo.svg"
-        />
-        <GlobalSvg
-          href={githubLink}
-          external
-          iconPath="/assets/images/svg/GithubLogo.svg"
-        />
-      </div>
-    </article>
+    <>
+      {job === "Front End Developer" ? (
+        <div className={styles.authors}>
+          <img src={picture} alt={name} className={styles.authors__picture} />
+
+          <p className={styles.authors__name}> {name} </p>
+          <p className={styles.authors__job}>{job} </p>
+
+          <div className={styles.authors__socialMedia}>
+            <GlobalSvg
+              href={linkedinLink}
+              external
+              iconPath="/assets/images/svg/LinkedinLogo.svg"
+            />
+            <GlobalSvg
+              href={githubLink}
+              external
+              iconPath="/assets/images/svg/GithubLogo.svg"
+            />
+          </div>
+        </div>
+      ) : (
+        <div className={styles.inspiredBy}>
+          <p className={styles.inspiredBy__text}> Design inspired by: </p>
+          <img src={picture} alt={name} className={styles.authors__picture} />
+
+          <p className={styles.inspiredBy__name}>{name} </p>
+          <a
+            href="https://www.uplabs.com/posts/pokedex-app"
+            className={styles.inspiredBy__link}
+          ></a>
+          <div className={styles.authors__socialMedia}>
+            <GlobalSvg
+              href={githubLink}
+              external
+              iconPath="/assets/images/svg/FigmaLogo.svg"
+            />
+            <GlobalSvg
+              href={linkedinLink}
+              external
+              iconPath="/assets/images/svg/Briefcase.svg"
+            />
+          </div>
+        </div>
+      )}
+    </>
   );
 }
