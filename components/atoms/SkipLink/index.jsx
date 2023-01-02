7fd6eab345e30266595cfb40d85ddedaca724f6e
elainemattjevt <elaine.mattje@valtech.com>
2023-01-02 10:07:06 -0300

    About and skip link

---
+++ components/atoms/SkipLink/index.jsx
@@ -0,0 +1,13 @@
+import styles from "./styles.module.scss";
+
+export default function SkipLink() {
+  return (
+    <>
+      <div aria-label="skip link">
+        <a className={styles.skipLink} href="#content">
+          Skip to content
+        </a>
+      </div>
+    </>
+  );
+}
