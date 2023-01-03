38a064be59a4ced700419e206baa6d74f9e031b4
kelly-cesario_valtech <kellycesario@gmail.com>
2023-01-03 14:55:41 -0300

    Fix: delete useOceanPage.js

--- store/useOceanPage.js
+++
@@ -1,8 +0,0 @@
-import create from "zustand";
-
-const useOceanPage = create((set) => ({
-  page: "teste",
-  setPage: (page) => set((state) => ({ page: page })),
-}));
-
-export default useOceanPage;
